7cd5d4f34b1685a52810a1e517d598f9e806f813
Ally4 <el.ally741@gmail.com>
2019-07-13 03:03:23 +0200

    chore(correction): problem on the deployment [ch#167268663]

--- server/app.js
+++ server/app.js
@@ -9,11 +9,12 @@ import router from './routers/routers';
 const app = express();
 
 app.use(express.json());
+
 app.use(bodyParser.json());
 
 app.use('/api/v1/', router);
 
-const port = process.env.PORT || 1000;
+const port = process.env.PORT || 2000;
 
 app.listen(port, () => { console.log(`app is listening on ${port}...`); });
 
