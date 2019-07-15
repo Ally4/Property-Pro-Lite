8eecf08cbe3a4e116985d37513d549392e8e3e53
Ally4 <el.ally741@gmail.com>
2019-07-15 21:32:26 +0200

    feature(price):the interval of the price [ft#167302082]

---
+++ server/controllers/createPosts.js
@@ -0,0 +1,42 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable quotes */
+/* eslint-disable comma-spacing */
+/* eslint-disable key-spacing */
+/* eslint-disable indent */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable no-trailing-spaces */
+import moment from 'moment';
+import posts from '../models/posts';
+import users from '../models/users';
+import validateAd from '../validators/posts';
+
+const Post = (req, res) => {
+  const { error } = validateAd.validation(req.body);
+  if (error) {
+    return res.status(400).json({ status: 400, error: error.details[0].message });
+  }
+
+  const id = parseInt(posts.length + 1, 10);
+  const {
+    email, type, state, city, address, price,
+  } = req.body;
+
+  if (req.body.price <= 1000) {
+return res.status(400).json({status: 400, message:"Not allowed to post, the price is low" }); 
+  }
+
+  const newPost = {
+    id, email, status:("Available") , type, state, city, address, price,
+  };
+
+
+  posts.push(newPost);
+  return res.status(201).json({ status: 'success', data: newPost });
+};
+
+export default Post;
---
+++ server/test/myTest.js
@@ -0,0 +1,91 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable arrow-spacing */
+/* eslint-disable eol-last */
+/* eslint-disable comma-dangle */
+/* eslint-disable padded-blocks */
+/* eslint-disable semi */
+/* eslint-disable key-spacing */
+/* eslint-disable no-undef */
+/* eslint-disable quotes */
+/* eslint-disable comma-spacing */
+/* eslint-disable indent */
+import chai from "chai";
+import chaiHttp from "chai-http";
+import app from "../app";
+
+chai.use(chaiHttp);
+chai.should();
+
+
+describe("User tests", () => {
+    it("User should be able to login", (done) => {
+        chai.request(app).post("/api/v1/auth/signin").send({
+            email: "admin@gmail.com",
+            password: "Abaaba1"
+        }).end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.an("object");
+            res.body.should.be.property('data');
+            done();
+        })
+    });
+
+    it("User should not be able to login when no email", (done) => {
+        chai.request(app).post("/api/v1/auth/signin").send({
+            email: "",
+            password: "Abaaba1"
+        }).end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.an("object");
+            done();
+        })
+    });
+
+    it("User should not be able to login when wrong password", (done) => {
+        chai.request(app).post("/api/v1/auth/signin").send({
+            email: "admin@gmail.com",
+            password: "A"
+        }).end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.an("object");
+            done();
+        })
+    });
+
+
+    it("User should not be able to login when wrong password and wrong email", (done) => {
+        chai.request(app).post("/api/v1/auth/signin").send({
+            email: "adminuu@gmail.com",
+            password: "A"
+        }).end((err, res) => {
+            res.should.have.status(404);
+            res.body.should.be.an("object");
+            done();
+        })
+    });
+
+    it("Should not be able to login", (done) => {
+        chai.request(app).post("/api/v1/auth/signin").send({
+            email: "sdfsf@sfs",
+            password: "sfgfgsfsf"
+        })
+            .end((err, res) => {
+                res.should.has.status(404);
+                done();
+            });
+
+    })
+    it("Should not be able to login when no e-mail and no password", (done) => {
+        chai.request(app).post("/api/v1/auth/signin").send({
+            email: "",
+            password: ""
+        })
+            .end((err, res) => {
+                res.should.has.status(404);
+                done();
+            });
+
+    })
+
+})
